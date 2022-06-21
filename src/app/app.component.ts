83aa1480357c261c59f27e4077a8b58ba5227418
abdullah.umar <abdullah.umar@axiatadigitallabs.com>
2022-06-21 10:02:55 +0700

    integrate with line/liff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.initLine();
+    this.getSignature();
   }
 
   initLine(): void {
@@ -40,94 +41,16 @@ export class AppComponent implements OnInit {
       this.pictureUrl = profile.pictureUrl;
       this.statusMessage = profile.statusMessage;
       this.userId = profile.userId;
-      // this.getFriendship();
     }).catch(err => console.error(err));
   }
 
-  notificationToken() {
-    this.http.post('https://api.line.me/message/v2/notifier/token', {
-      liffAccessToken: liff.getAccessToken()
-    }).subscribe(resp => {
-      console.log(resp);
-    })
-  }
+  getSignature() {
+    const crypto = require('crypto');
+    const channelSecret = 'bb9d7b27df04c1a30b8a2870a7e56f9f';
+    const body = '...';
+    const signature = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64');
 
-  sendMessage() {
-    liff
-    .sendMessages([
-      {
-        type: "text",
-        text: "Hello, World!",
-      },
-    ])
-    .then(() => {
-      console.log("message sent");
-    })
-    .catch((err) => {
-      console.log("error", err);
-    });
-  }
-
-  sendTarget() {
-    liff.shareTargetPicker(
-      [
-        {
-            "type": "text",
-            "text": "Have you decided on your order?",
-            "quickReply": {
-                "items": [
-                    {
-                        "type": "action",
-                        "action": {
-                            "type": "uri",
-                            "label": "Menu",
-                            "uri": "https://example.com/menu"
-                        }
-                    },
-                    {
-                        "type": "action",
-                        "action": {
-                            "type": "uri",
-                            "label": "Phone order",
-                            "uri": "tel:09001234567"
-                        }
-                    },
-                    {
-                        "type": "action",
-                        "action": {
-                            "type": "uri",
-                            "label": "Recommend to friend",
-                            "uri": "https://line.me/R/nv/recommendOA/@linedevelopers"
-                        }
-                    }
-                ]
-            }
-        }
-    ],
-      {
-        isMultiple: true,
-      }
-    )
-    .then(function (res) {
-      if (res) {
-        // succeeded in sending a message through TargetPicker
-        console.log(`[${res.status}] Message sent!`)
-      } else {
-        const [majorVer, minorVer] = (liff.getLineVersion() || "").split('.');
-        if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
-          // LINE 10.3.0 - 10.10.0
-          // Old LINE will access here regardless of user's action
-          console.log('TargetPicker was opened at least. Whether succeeded to send message is unclear')
-        } else {
-          // LINE 10.11.0 -
-          // sending message canceled
-          console.log('TargetPicker was closed!')
-        }
-      }
-    }).catch(function (error) {
-      // something went wrong before sending a message
-      console.log('something wrong happen')
-    })
+    console.log(signature);
   }
 
   logout(): void {
